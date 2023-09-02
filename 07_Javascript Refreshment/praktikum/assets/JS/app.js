7dfbf6b95f84ba4070b230681834910111c74491
nandopuntodewo <deanandropuntodewo@students.unnes.ac.id>
2023-09-03 05:50:41 +0700

    update tugas idata structure

--- 07_Javascript Refreshment/praktikum/assets/JS/app.js
+++ 07_Javascript Refreshment/praktikum/assets/JS/app.js
@@ -1,56 +1,37 @@
-
-const form = document.getElementById('formCreateProduct')
-
+const form = document.getElementById('formCreateProduct');
 
 form.addEventListener('submit', (e) => {
     let specialchars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
-    const productName = document.getElementById('productName')
-    const productPrice = document.getElementById('productPrice')
-    const option = document.getElementById('productCategory')
-    const formFile = document.getElementById('formFile')
-    const Description = document.getElementById('Description')
-    var radio1 = document.getElementById('Brandnew')
-    var radio2 = document.getElementById('Secondhand')
-    var radio3 = document.getElementById('Refurbished')
-
+    const productName = document.getElementById('productName');
+    const productPrice = document.getElementById('productPrice');
+    const option = document.getElementById('productCategory');
+    const formFile = document.getElementById('formFile');
+    const Description = document.getElementById('Description');
+    const productFreshness = document.querySelector('input[name="productStatus"]:checked');
 
     if (productName.value === "" || productName.value == null){
-        alert ("Please enter a valid product name");
+        alert("Please enter a valid product name");
         return false;
-    }else
-
-    if (productName.value.length > 25 || productName.value.length > 10){
-        alert ("Last Name must not exceed 25 characters.");
+    } else if (productName.value.length > 25 || productName.value.length > 10){
+        alert("Last Name must not exceed 25 characters.");
         return false;
-    }else
-
-    if (productName.value.match(specialchars)) {
-        alert("Prodcut name must not contain symbols")
+    } else if (productName.value.match(specialchars)) {
+        alert("Product name must not contain symbols");
         return false;
-    }else
-
-    if (option.value == ""){
-        alert ("Product Category field must be filled in");
+    } else if (option.value == ""){
+        alert("Product Category field must be filled in");
         return false;
-    }else
-
-    if (formFile.files.length === 0 ){ 
-        alert("Product image field must be filled in")
+    } else if (formFile.files.length === 0 ){ 
+        alert("Product image field must be filled in");
         return false;
-    }else
-
-    if (radio1.checked !== true && radio2.checked !== true && radio3.checked !== true){
-        alert ("Product freshness field must be filled in")
+    } else if (!productFreshness) {
+        alert("Product freshness field must be filled in");
         return false;
-    }else
-
-    if (Description.value === "" || Description.value == null){
-        alert("Additional Description field must be filled in")
-        return false
-    }else
-
-    if (productPrice.value === "" || productPrice.value == null){
-        alert("Please enter a valid product price")
+    } else if (Description.value === "" || Description.value == null){
+        alert("Additional Description field must be filled in");
+        return false;
+    } else if (productPrice.value === "" || productPrice.value == null){
+        alert("Please enter a valid product price");
         return false;
     }
-})
+});
--- 08_Introduction Data Structures (javascript)/praktikum/assets/JS/app.js
+++ 08_Introduction Data Structures (javascript)/praktikum/assets/JS/app.js
@@ -1,3 +1,54 @@
+const form = document.getElementById('formCreateProduct');
+
+function validateForm() {
+    let specialchars = /[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/;
+    const productName = document.getElementById('productName');
+    const productPrice = document.getElementById('productPrice');
+    const option = document.getElementById('productCategory');
+    const formFile = document.getElementById('formFile');
+    const Description = document.getElementById('Description');
+    const productFreshness = document.querySelector('input[name="productStatus"]:checked');
+
+    if (productName.value === "" || productName.value == null){
+        alert("Please enter a valid product name");
+        return false;
+    } else if (productName.value.length > 25 || productName.value.length > 10){
+        alert("Last Name must not exceed 25 characters.");
+        return false;
+    } else if (productName.value.match(specialchars)) {
+        alert("Product name must not contain symbols");
+        return false;
+    } else if (option.value == ""){
+        alert("Product Category field must be filled in");
+        return false;
+    } else if (formFile.files.length === 0 ){ 
+        alert("Product image field must be filled in");
+        return false;
+    } else if (!productFreshness) {
+        alert("Product freshness field must be filled in");
+        return false;
+    } else if (Description.value === "" || Description.value == null){
+        alert("Additional Description field must be filled in");
+        return false;
+    } else if (productPrice.value === "" || productPrice.value == null){
+        alert("Please enter a valid product price");
+        return false;
+    }
+
+    return true; // Kembalikan true jika validasi berhasil
+}
+
+// Menambahkan event listener untuk validasi saat mengirimkan formulir
+form.addEventListener('submit', (e) => {
+    if (!validateForm()) {
+        e.preventDefault(); // Mencegah pengiriman formulir jika validasi gagal
+    }
+});
+
+// Menambahkan event listener untuk menangani pengiriman formulir
+document.getElementById('formCreateProduct').addEventListener('submit', handleSubmit);
+
+
 // Inisialisasi array untuk menyimpan data produk
 const products = [];
 
@@ -52,11 +103,12 @@ function handleSubmit(event) {
     document.getElementById('formCreateProduct').reset();
 }
 
-// Menambahkan event listener untuk menangani pengiriman formulir
-document.getElementById('formCreateProduct').addEventListener('submit', handleSubmit);
-
 // Fungsi untuk menghapus produk terakhir
-function deleteLastProduct() {
+function deleteLastProduct(event) {
+    if (event) {
+        event.preventDefault(); // Mencegah tombol "delete" mengirimkan permintaan HTTP
+    }
+    
     if (products.length > 0) {
         products.pop(); // Menghapus produk terakhir dari array
         const tableBody = document.getElementById('productTableBody');
@@ -64,5 +116,43 @@ function deleteLastProduct() {
     }
 }
 
+
 // Menambahkan event listener untuk tombol delete
-document.getElementById('deleteButton').addEventListener('click', deleteLastProduct);
+document.getElementById('deleteButton').addEventListener('click', (e) => deleteLastProduct(e));
+
+
+
+
+// Fungsi untuk melakukan pencarian pada tabel
+function searchProduct(event) {
+    if (event) {
+        event.preventDefault(); // Mencegah tombol "delete" mengirimkan permintaan HTTP
+    }
+    const searchInput = document.getElementById('searchInput').value.toLowerCase(); // Mendapatkan nilai input pencarian dan mengonversi menjadi lowercase
+    const tableBody = document.getElementById('productTableBody');
+    const rows = tableBody.getElementsByTagName('tr'); // Mengambil semua baris dalam tabel
+
+    // Loop melalui setiap baris dalam tabel
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        const cells = row.getElementsByTagName('td'); // Mengambil sel-sel dalam baris
+
+        // Loop melalui setiap sel dalam baris (mulai dari indeks 1 karena indeks 0 adalah nomor urut)
+        for (let j = 1; j < cells.length; j++) {
+            const cell = cells[j];
+            const cellText = cell.textContent.toLowerCase(); // Mengambil teks dalam sel dan mengonversi menjadi lowercase
+
+            // Memeriksa apakah teks dalam sel cocok dengan teks pencarian
+            if (cellText.includes(searchInput)) {
+                row.style.display = ''; // Menampilkan baris jika ada kesesuaian
+                break; // Keluar dari loop setelah menemukan kesesuaian dalam baris
+            } else {
+                row.style.display = 'none'; // Menyembunyikan baris jika tidak ada kesesuaian
+            }
+        }
+    }
+}
+// Menambahkan event listener pada input pencarian untuk mendeteksi perubahan nilai
+document.getElementById('searchInput').addEventListener('input', function () {
+    searchProduct(); // Menjalankan fungsi pencarian tanpa alert
+});
